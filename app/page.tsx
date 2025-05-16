6c96046c97d9a84ee46f9b1bab25bba27cd71253
Bean <bean1730137@gmail.com>
2025-05-16 22:30:16 +0800

    feat: origin

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   // Action we want to execute in the Blink
   const { blink, isLoading } = useBlink({
-    url: "evm-action:http://localhost:3000/api/actions/donate-mon",
+    url: "evm-action:https://blink-starter-monad.vercel.app/api/actions/donate-mon",
   });
 
   return (
